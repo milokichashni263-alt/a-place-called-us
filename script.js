bdb5cc4fc0d72dcc70a3aea7a318611a00f0f43c
milokichashni263-alt <milokichashni263@gmail.com>
2026-08-06 00:59:13 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+const passwordInput = document.getElementById("password");
+const unlockButton = document.getElementById("unlockButton");
+const welcomeScreen = document.getElementById("welcomeScreen");
+
+const correctPassword = "081929";
+
+unlockButton.addEventListener("click", unlock);
+
+passwordInput.addEventListener("keydown", (e)=>{
+    if(e.key==="Enter"){
+        unlock();
+    }
+});
+
+function unlock(){
+
+    if(passwordInput.value===correctPassword){
+
+        welcomeScreen.classList.add("show");
+
+    }else{
+
+        passwordInput.animate([
+            {transform:"translateX(-8px)"},
+            {transform:"translateX(8px)"},
+            {transform:"translateX(-8px)"},
+            {transform:"translateX(8px)"},
+            {transform:"translateX(0px)"}
+        ],{
+            duration:350
+        });
+
+        passwordInput.value="";
+        passwordInput.placeholder="Hmm... that's not our little secret.";
+    }
+
+}
